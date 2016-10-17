03678e09459d0dcf40b142def1ec1e42ec8131f4
Alma Maizels <aymaiz@yahoo.com>
2016-10-17 11:17:20 -0700

    Steps 1 - 4 at lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get "/auth/:provider/callback" =>  "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
