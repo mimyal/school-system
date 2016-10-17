4412d039a1f874e35938f98bfd2fc01f618ae449
Alma Maizels <aymaiz@yahoo.com>
2016-10-17 12:17:08 -0700

    lecture notes/checklist done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,35 @@
 class SessionsController < ApplicationController
+  def index
+    if  session[:user_id].nil?
+      redirect_to login_failure_path
+    else
+      @user = User.find(session[:user_id]) # < recalls the value set in a previous request
+      # implicit call to
+      render :index
+    end
+  end
+
   def create
     auth_hash = request.env['omniauth.auth']
-    # raise
+    redirect to login_failure_path unless auth_hash['uid']
+
+    @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+    if @user.nil?
+      # User doesn't match anything in the DB.
+      # Attempt to create a new user.
+      @user = User.build_from_github(auth_hash)
+      render :creation_failure unless @user.save
+    end
+
+    # Save the user ID in the session
+    session[:user_id] = @user.id
+
+    redirect_to sessions_path
   end
+
+  def login_failure
+
+  end
+
+
 end
